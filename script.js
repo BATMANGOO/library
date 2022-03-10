4612bd53b3962b8da96e644ca6806809bc7fe248
Prabhjot Singh <pjsingh1359@gmail.com>
2022-03-09 22:12:44 -0800

    display created books

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let addBtn = document.querySelector('.add-btn');
 let appBody = document.querySelector('.cards-container');
 let cancelBtn = document.querySelector('.cancel-btn');
 let submitBtn = document.querySelector('.submit-btn');
+let deleteBtn = document.querySelector('.delete-btn');
+let books = document.querySelector('.book-collection');
 
 let myLibrary = [];
 
@@ -15,14 +17,51 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
   let titleVal = form.elements.title.value;
   let authorVal = form.elements.author.value;
   let pagesVal = form.elements.pages.value;
   let readVal = form.elements.read.checked;
-  let newBook = new Book(titleVal, authorVal, pagesVal, readVal);
-  myLibrary.push(newBook);
-  console.log(myLibrary);
+  if (checkValidity(titleVal, authorVal, pagesVal)) {
+    let newBook = new Book(titleVal, authorVal, pagesVal, readVal);
+    myLibrary.push(newBook);
+    form.reset();
+    console.log(myLibrary);
+  } else {
+    alert('Please fill in all fields');
+  };
+}
+
+function checkValidity(title, author, pages) {
+  if ((title === '' || myLibrary.some(obj => obj.title == title)) || author === '' || pages === '') {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+function displayBooks() {
+  myLibrary.map(input => {
+    let newCard = document.createElement('div');
+    newCard.classList.add('book');
+    newCard.innerHTML = `
+    <p class="title">${input.title}</p>
+    <p class="author">${input.author}</p>
+    <p class="pages">
+      pages: <span class="number-of-pages">${input.pages}</span>
+    </p>
+    <button class="read book-button">${input.read ? 'Read' : 'Unread'}</button>
+    <button class="delete-btn" onCLick="deleteBook()">Delete</button>
+    `
+    books.appendChild(newCard);
+    console.log(books.children);
+  });
+}
+
+function deleteBook() { // Fix this
+  deleteBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log(e.target.parentElement);
+  });
 }
 
 addBtn.addEventListener('click', function(e) {
@@ -33,6 +72,7 @@ addBtn.addEventListener('click', function(e) {
 submitBtn.addEventListener('click', function(e) {
   e.preventDefault();
   addBookToLibrary();
+  displayBooks();
   formContainer.classList.add('form-hide');
   appBody.classList.remove('grey-out');
 });
@@ -42,3 +82,6 @@ cancelBtn.addEventListener('click', function(e) {
   formContainer.classList.add('form-hide');
   appBody.classList.remove('grey-out');
 });
+
+
+
